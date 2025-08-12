56270d796aaa52efb601d2352696ba1f60f4190c
freeecondata <freeecondata@gmail.com>
2025-08-11 22:30:56 -0500

    update repo

---
+++ week-04/preparar.R
@@ -0,0 +1,30 @@
+
+
+
+## librerias
+library(tidyverse)
+require(rio)
+require(data.table)
+
+## Crear datos limpios
+df <- import("week-04/input/RUES_clean_dirv2.rds" , setclass = "data.table")
+df <- df[cod_mpio==76001 & year_renueva>=2023]
+df <- df[,.(dpto_name,mpio_name,unique_id,razon_social,year_matricula,year_cancela,year_renueva,ciiu1,ciiu2,ciiu3,ciiu4)]
+
+## Determinar el 10% de filas a contaminar
+set.seed(123)
+filas_error <- sample(1:nrow(df) , size = round(nrow(df) * 0.05))
+
+## crear variables
+df$`Activos M.` <- round(rnorm(nrow(df),10000,2500))
+df$`Ingresos M.` <- round(rnorm(nrow(df),5000,2000))
+df$`Pasivos M.` <- round(rnorm(nrow(df),-2000,2000))
+df$`Patrimonio M.` <- df$`Activos M.` + df$`Pasivos M.`
+
+## agregar error
+df$`Activos M.`[filas_error] <- 999999
+
+## export datos
+export(df , "week-04/output/empresas_cali.csv")
+
+
